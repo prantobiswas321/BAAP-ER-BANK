2556ff695d805ddf867baa96cad2e938abbd2c1a
prantobiswas321 <rahulbd321@gmail.com>
2021-08-16 22:46:08 +0600

    edited

--- js/banking.js
+++ js/banking.js
@@ -8,17 +8,14 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
 
     // update deposit total
     const depositTotal = document.getElementById('deposit-total');
-
     const previousDepositText = depositTotal.innerText;
     const previousDepositAmount = parseFloat(previousDepositText);
 
     const newDepositTotal = previousDepositAmount + newDepositAmount;
-
     depositTotal.innerText = newDepositTotal;
 
     // update account balance
     const balanceTotal = document.getElementById('balance-total');
-
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
 
@@ -35,21 +32,17 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmountText = withdrawInput.value;
     const newWithdrawAmount = parseFloat(withdrawAmountText);
-    console.log(newWithdrawAmount);
 
     // update withdraw total
     const withdrawTotal = document.getElementById('withdraw-total');
-
     const previousWithdrawText = withdrawTotal.innerText;
     const previousWithdrawAmount = parseFloat(previousWithdrawText);
 
     const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
-
     withdrawTotal.innerText = newWithdrawTotal;
 
     // update account balance
     const balanceTotal = document.getElementById('balance-total');
-
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
 
